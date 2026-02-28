b6c698c93b54b907ae14a3fbabd1cb13c3ca4886
huynyojin <96348327+huynyojin@users.noreply.github.com>
2026-02-28 18:33:29 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,5 @@ import requests
 url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
 
 response = requests.get(url)
-data = response.json()
-
-price = data["price"]
-
-print(f"🔥 BTC Price: {price} USDT")
+print("Status code:", response.status_code)
+print("Raw response:", response.text)
