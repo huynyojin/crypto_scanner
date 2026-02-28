608b898fa78d2d99aa74dd7c5a7d4b64e33733d9
huynyojin <96348327+huynyojin@users.noreply.github.com>
2026-02-28 18:25:57 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
-print("Hello from GitHub server 🚀")
+import requests
+
+url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+
+response = requests.get(url)
+data = response.json()
+
+price = data["price"]
+
+print(f"🔥 BTC Price: {price} USDT")
